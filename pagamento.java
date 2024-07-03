9b827ff04a5384e80d1fb33b1c8d4cd2ad2912d9
George-Rot <george.rother@outlook.com>
2024-07-03 08:54:56 -0300

    Several changes

--- pagamento.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class pagamento {
-    private long code;
-    private long codeAssinatura;
-    private float valorPago;
-    private LocalDate dataPagamento;
-}
